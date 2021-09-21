ecd4fe5813489022be1706b190e3db5f604cdcc1
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-09-21 13:17:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ pip install ipaddress
 sudo gem install colorize 
 sudo gem install open-uri 
 sudo gem install packetgen 
+sudo apt-get install libpcap-dev
 gem install colorize
 gem install open-uri
 gem install packetgen
